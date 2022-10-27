1417a0c86263183466deede7a47f0f367178e65e
Jeferson <jefersontca@gmail.com>
2022-10-27 17:54:28 +0000

    projeto

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          VASCO !!!, edit <code>src/App.js</code> and save to reload.
+          VASCO SOBE !!!, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
